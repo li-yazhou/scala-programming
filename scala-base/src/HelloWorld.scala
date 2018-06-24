05e7ac259d759e0c465aaa65c7c40389d94cb960
lyz0907 <yazhou6543@gmail.com>
2018-06-24 10:15:42 +0800

    init commit

---
+++ scala-base/src/HelloWorld.scala
@@ -0,0 +1,13 @@
+/**
+  * <p>Description: </p>
+  *
+  * @author lyz
+  * @since 2018/6/24 10:11
+  * @version 0.0.1
+  */
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        println("hello world")
+    }
+
+}
